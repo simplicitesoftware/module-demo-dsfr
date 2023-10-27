40bbb891e0fdb98fdc7121e8f2ee571951ea7410
David AZOULAY <dazoulay@simplicite.fr>
2023-10-27 17:41:51 +0200

    Modernized syntax

--- others/src/main.js
+++ others/src/main.js
@@ -12,67 +12,82 @@ import simplicite from 'simplicite';
 
 const app = simplicite.session(process.env.WEBPACK_APP_URL ? { url: process.env.WEBPACK_APP_URL, debug: true } : {});
 
-app.info('Version: ' + simplicite.constants.MODULE_VERSION);
+app.info(`Version: ${simplicite.constants.MODULE_VERSION}`);
 app.debug(app.parameters);
 
 let prd;
 
 function elt(id, html) {
-	const el = document.getElementById('web-demo-' + id);
-	if (el && html) el.innerHTML = html;
+	const el = document.getElementById(`dsfr-demo-${id}`);
+	if (el && typeof html === 'string') el.innerHTML = html;
 	return el;
 }
 
-app.login({ username: 'website', password: 'simplicite' }).then(function(user) {
-	app.debug('Logged in as ' + user.login);
-	return app.getGrant();
-}).then(function(grant) {
-	app.debug(grant);
-	elt('message', 'Hello ' + grant.getLogin());
-	prd = app.getBusinessObject('DemoProduct');
-	return prd.getMetaData();
-}).then(function(metadata) {
-	app.debug(metadata);
-	elt('product-add').onclick = function() {
-		prd.getForCreate().then(function() {
-			console.log(prd.item);
+function error(err) {
+	app.error(err);
+	elt('message',
+		`<div class="fr-alert fr-alert--error">\
+			<div class="fr-container">\
+				<div class="fr-alert__body">\
+					<h3 class="fr-alert__title">Erreur</h3>\
+					<p>${err.message || err.messages.join(',')}</p>\
+				</div>\
+			</div>\
+		</div>`);
+}
+
+async function display() {
+	const list = await prd.search({ demoPrdAvailable: true }, { inlineDocuments: [ 'demoPrdPicture' ] });
+	let l = '';
+	for (const item of list) {
+		const img = item.demoPrdPicture ? `<img alt="Picture" src="data:${item.demoPrdPicture.mime};base64,${item.demoPrdPicture.content}"/>` : '';
+		l += `<div class="fr-col-sm-12 fr-col-md-6 fr-col-lg-4">\
+			<div class="fr-tile fr-enlarge-link fr-tile--horizontal">\
+				<div class="fr-tile__body">\
+					<h4 class="fr-tile__title">${item.demoPrdName}</h4>\
+					<h5 class="fr-tile__title">${item.demoPrdReference} (${prd.getFieldListValue('demoPrdType', item)})</h5>\
+					<p class="fr-tile__desc">${item.demoPrdDescription}</p>\
+				</div>\
+				<div class="fr-tile__img">${img}</div>\
+			</div>\
+		</div>`;
+	}
+	elt('products', `<div class="fr-grid-row fr-grid-row--gutters">${l}</div>`);
+}
+
+async function save() {
+	try {
+		elt('message', '');
+		await prd.save();
+		display();
+	} catch (err) { error(err); }
+}
+
+(async () => {
+	try {
+		const user = await app.login({ username: 'website', password: 'simplicite' });
+		app.debug(`Connecté en tant que ${user.login}`);
+		const grant = await app.getGrant();
+		app.debug(grant);
+		app.info(`Bonjour ${grant.getFirstname()} ${grant.getLastname()}`);
+		prd = app.getBusinessObject('DemoProduct');
+		const metadata = await prd.getMetaData();
+		app.debug(metadata);
+		elt('product-add').onclick = async () => {
+			const item = await prd.getForCreate();
+			app.debug(item);
 			prd.item.demoPrdSupId = 1;
 			prd.item.demoPrdType = 'OTHER';
 			prd.item.demoPrdReference = elt('product-ref').value;
 			prd.item.demoPrdName = elt('product-name').value;
 			var file = elt('product-picture').files[0];
-			console.log(file);
-			var fr = new FileReader();
-			fr.onload = function() {
-				if (fr.result) {
-					prd.item.demoPrdPicture = { id: 0, name: file.name, type: file.type, content: fr.result };
-					console.log(prd.item);
-					prd.save().then(function() {
-						console.log(prd.item);
-					});
-				}
-			};
-			fr.readAsDataURL(file);
-		});
-	};
-	return prd.search({ demoPrdAvailable: true }, { inlineDocuments: [ 'demoPrdPicture' ] });
-}).then(function(list) {
-	app.debug(list);
-	let l = '<div class="fr-grid-row fr-grid-row--gutters">';
-	for (const item of list)
-		l += '<div class="fr-col-sm-12 fr-col-md-6 fr-col-lg-4">' +
-				'<div class="fr-tile fr-enlarge-link fr-tile--horizontal">' +
-					'<div class="fr-tile__body">' +
-						'<h4 class="fr-tile__title">' + item.demoPrdName + '</h4>' +
-						'<h5 class="fr-tile__title">' + item.demoPrdReference + ' (' + prd.getFieldListValue('demoPrdType', item) + ')</h5>' +
-						'<p class="fr-tile__desc">' + item.demoPrdDescription + '</p>' +
-					'</div>' +
-					'<div class="fr-tile__img"><img alt="Picture" src="data:' + item.demoPrdPicture.mime + ';base64,' + item.demoPrdPicture.content + '"/></div>' +
-				'</div>' +
-			'</div>';
-	l += '</div>';
-	elt('products', l);
-}).catch(function(err) {
-	app.log(err);
-	elt('message', '<div class="error">Error: ' + err.message + '</div>');
-});
+			if (file) {
+				prd.item.demoPrdPicture = await new simplicite.Doc().load(file);
+				app.debug(prd.item);
+				save();
+			} else
+				save();
+		};
+		display();
+	} catch (err) { error(err); }
+})();
