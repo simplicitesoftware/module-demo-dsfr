1fe47cce7c17e741ec670a3d24726429a2d8682e
David AZOULAY <dazoulay@simplicite.fr>
2024-01-16 21:37:08 +0100

    Bump deps

--- others/.stylelintrc.js
+++ others/.stylelintrc.js
@@ -1,8 +1,6 @@
 module.exports = {
 	'customSyntax': 'postcss-less',
 	'rules': {
-		'indentation': 2,
-		'declaration-empty-line-before': 'never',
-		'number-leading-zero': 'never'
+		'declaration-empty-line-before': 'never'
 	}
 };
