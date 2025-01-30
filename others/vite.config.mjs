ecd059b96929d4bfde64c5ff5ac4b856e9797085
David AZOULAY <dazoulay@simplicite.fr>
2025-01-30 16:41:34 +0100

    Bump dev deps + dev server allowed from any host

--- others/vite.config.mjs
+++ others/vite.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: './',
   server: {
+    host: '0.0.0.0',
+    allowedHosts: true,
     port: 3000
   },
   preview: {
+    host: '0.0.0.0',
+    allowedHosts: true,
     port: 3000
   }
 });
